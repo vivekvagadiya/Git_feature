fd754b1bf11926e2ca44eb89680180fcb88146e6
James <jamescameron@gmail.com>
2025-03-13 12:45:33 +0530

    reverse string add

--- local.js
+++ local.js
@@ -6,3 +6,11 @@ function sortArray(arr) {
 const array = [5, 3, 8, 1, 2];
 const sortedArray = sortArray(array);
 console.log(sortedArray); // Output: [1, 2, 3, 5, 8]
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+// Example usage:
+const originalString = "hello";
+const reversedString = reverseString(originalString);
+console.log(reversedString); // Output: "olleh"
