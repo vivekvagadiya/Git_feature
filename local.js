939b545ef634edba2ee983312da7b6b2a6190c04
James <jamescameron@gmail.com>
2025-03-13 12:32:39 +0530

    local file add

---
+++ local.js
@@ -0,0 +1,8 @@
+function sortArray(arr) {
+    return arr.sort((a, b) => a - b);
+}
+
+// Example usage:
+const array = [5, 3, 8, 1, 2];
+const sortedArray = sortArray(array);
+console.log(sortedArray); // Output: [1, 2, 3, 5, 8]
